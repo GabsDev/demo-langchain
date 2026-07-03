8e89796bfa46d286f60a0169049d7fb8a725aac7
Luis Avila <gavila87@gmail.com>
2026-07-03 03:26:41 -0600

    langGraph

---
+++ Tema 4 - LangGraph/helpdesk_system/config.py
@@ -0,0 +1,3 @@
+CHROMADB_PATH = "C:\\Projects\\demo_langchain\\Tema 4 - LangGraph\\helpdesk_system\\chroma_db"
+DOCS_PATH = "C:\\Projects\\demo_langchain\\Tema 4 - LangGraph\\helpdesk_system\\docs"
+EMBEDDINGS_MODEL = "text-embedding-3-large"
